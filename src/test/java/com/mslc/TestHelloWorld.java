3a50ba89ca5f2ebf5de98d2c326450130d8767b5
HebeKuo <hebekuo@systex.com>
2019-01-31 16:47:28 +0800

    Update TestHelloWorld.java

--- src/test/java/com/mslc/TestHelloWorld.java
+++ src/test/java/com/mslc/TestHelloWorld.java
@@ -8,33 +8,37 @@ import org.junit.Test;
 public class TestHelloWorld {
 
 	private HelloWorld h;
-	
+
 	   @Before
-	   public void setUp() throws Exception 
+	   public void setUp() throws Exception
 	   {
 	      h = new HelloWorld();
 	   }
 
 	   @Test
-	   public void testHelloEmpty() 
-	   {
-	      assertEquals(h.getName(),"");
-	      assertEquals(h.getMessage(),"Hello!");
+	   public void testHelloEmpty() {
+		String name = "", msg = "Hello!";
+		h.setName(name);
+		assertEquals(h.getName(),name);
+		assertEquals(h.getMessage(),msg);
+		System.out.println("~~~~~~~~~~~~~test name:" + name + " ,msg:" + msg);
 	   }
-		
+
 	   @Test
-	   public void testHelloWorld() 
-	   {
-	      h.setName("World");
-	      assertEquals(h.getName(),"World");
-	      assertEquals(h.getMessage(),"Hello World!");
+	   public void testHelloWorld() {
+		String name = "World", msg = "Hello World!";
+		h.setName(name);
+		assertEquals(h.getName(),name);
+		assertEquals(h.getMessage(),msg);
+		System.out.println("~~~~~~~~~~~~~test name:" + name + " ,msg:" + msg);
 	   }
-	
+
 	   @Test
-	   public void testHelloWorld2() 
-	   {
-	      h.setName("World");
-	      assertEquals(h.getName(),"World");
-	      assertEquals(h.getMessage(),"Hello World!");
-	   }
+ 	   public void testHelloWorld2() {
+		 String name = "World2", msg = "Hello World2!";
+		 h.setName(name);
+		 assertEquals(h.getName(),name);
+		 assertEquals(h.getMessage(),msg);
+		 System.out.println("~~~~~~~~~~~~~test name:" + name + " ,msg:" + msg);
+ 	   }
 }
